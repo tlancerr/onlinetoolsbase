4a46c74c0dce5a44b0af7d3b1bc3442005f5a1d6
tlancerr <85990792+tlancerr@users.noreply.github.com>
2026-03-07 20:33:40 +0500

    Create page.tsx

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,38 @@
+async function getPost(slug: string) {
+  const res = await fetch(
+    `https://cms.onlinetoolsbase.com/wp-json/wp/v2/posts?slug=${slug}`,
+    { next: { revalidate: 3600 } }
+  );
+
+  const data = await res.json();
+  return data[0];
+}
+
+export default async function BlogPost({ params }: any) {
+
+  const post = await getPost(params.slug);
+
+  if (!post) {
+    return <div>Post not found</div>;
+  }
+
+  return (
+    <div className="container mx-auto max-w-3xl py-10">
+
+      <h1
+        className="text-4xl font-bold mb-6"
+        dangerouslySetInnerHTML={{
+          __html: post.title.rendered,
+        }}
+      />
+
+      <div
+        className="prose max-w-none"
+        dangerouslySetInnerHTML={{
+          __html: post.content.rendered,
+        }}
+      />
+
+    </div>
+  );
+}

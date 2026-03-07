0a4049424f5494f34cfb3a5e51c9788c930aac39
tlancerr <85990792+tlancerr@users.noreply.github.com>
2026-03-08 00:59:26 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+images: {
+  remotePatterns: [
+    {
+      protocol: "https",
+      hostname: "cms.onlinetoolsbase.com",
+    },
+  ],
+},
+  
   /* config options here */
   reactStrictMode: true,
   

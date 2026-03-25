49597836b8b1eecab3bb9639d207a9720f9e0bda
tlancerr <85990792+tlancerr@users.noreply.github.com>
2026-03-25 14:05:48 +0500

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 async function getPosts() {
   const res = await fetch(
     "https://cms.onlinetoolsbase.com/wp-json/wp/v2/posts?per_page=12&_embed=1",
-    { cache: "no-store" }
+    { next: { revalidate: 300 } }
   );
 
   if (!res.ok) {

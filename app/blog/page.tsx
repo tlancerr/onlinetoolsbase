4f0f75ff6aae9dd1d612a5f6700f1c8fb81217e9
tlancerr <85990792+tlancerr@users.noreply.github.com>
2026-07-18 16:16:46 +0500

    Update API endpoint for fetching blog posts

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -29,7 +29,7 @@ async function fetchWithTimeout(url: string) {
 
 async function getPosts() {
   return fetchWithTimeout(
-    "https://cms.onlinetoolsbase.com/wp-json/wp/v2/posts?per_page=12&_embed=1"
+    "https://cms.cottagestore.pk/wp-json/wp/v2/posts?per_page=12&_embed=1"
   );
 }
 

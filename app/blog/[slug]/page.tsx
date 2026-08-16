07bfdbf00cafbdc415ac686091d725ddddd34c05
tlancerr <85990792+tlancerr@users.noreply.github.com>
2026-08-16 16:40:30 +0500

    Fix URL in getPost function for fetching posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 async function getPost(slug: string) {
   const res = await fetch(
-    `https://cms.cottagestore.pk/wp-json/wp/v2/posts?slug=${encodeURIComponent(slug)}&_embed=1`,
+    `https://cms.cottagecore.pk/wp-json/wp/v2/posts?slug=${encodeURIComponent(slug)}&_embed=1`,
     { 
       method: 'GET',
       headers: {
